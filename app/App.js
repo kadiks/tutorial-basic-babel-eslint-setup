a7e10aceef69a662f0d941d1addf3fe6143b7df9
Jénaïc Cambré <jenaic@lilbigmen.com>
2016-08-14 21:04:42 +0200

    commit

---
+++ app/App.js
@@ -0,0 +1,7 @@
+class App {
+  constructor() {
+    console.log('Hello world!'); // eslint-disable-line no-console
+  }
+}
+
+const app = new App(); // eslint-disable-line no-unused-vars
---
+++ build/App.js
@@ -0,0 +1,11 @@
+'use strict';
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var App = function App() {
+  _classCallCheck(this, App);
+
+  console.log('Hello world!'); // eslint-disable-line no-console
+};
+
+var app = new App(); // eslint-disable-line no-unused-vars
